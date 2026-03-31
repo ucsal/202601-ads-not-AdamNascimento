645333f272850d534c9050c516efe14e7ed15ddc
AdamNascimento <adamsilva665@gmail.com>
2026-03-30 21:24:15 -0300

    fix: remover pastas desnecessarias do repositorio

--- CRUD em php/CRUD/navbar.php
+++
@@ -1,4 +0,0 @@
-<nav class="navbar navbar-dark bgdark"></nav>
-  <div class="container-md">
-    <a class="navbar-brand" href="#">CRUD - Adam</a>
-  </div>
--- spring.boot.apirest/src/main/java/br/com/adam/spring/boot/apirest/database/asd.java
+++
@@ -1,4 +0,0 @@
-package br.com.adam.spring.boot.apirest.database;
-
-public interface asd {
-}
